5fc54935ea8ff6ee705b7dcf176225fe050f2328
Davidbeckham2005 <dinhhoangkhamtb04@gmail.com>
2026-05-28 19:09:40 +0700

    feat: init budgets

---
+++ src/services/budgets.ts
@@ -0,0 +1,83 @@
+import { supabase } from "../lib/supabase";
+import type { ICreateBudget, IBudget, IUpdateBudget } from "../types/IBudget";
+
+export async function createBudget(payload: ICreateBudget, user_id: string) {
+    const { categories_ids, ...budgetData } = payload;
+    const { data: budget, error } = await supabase
+        .from('budgets')
+        .insert({
+            ...budgetData,
+            user_id,
+        })
+        .select()
+        .single();
+    if (error) throw error;
+    if (categories_ids.length > 0) {
+        const relations = categories_ids.map((category_id) => ({
+            budget_id: budget.id,
+            category_id
+        }))
+        const { error: relationError } = await supabase
+            .from('budget_categories')
+            .insert(relations);
+        if (relationError) throw relationError;
+    }
+    return budget;
+}
+
+export async function getBudgets(user_id: string) {
+    const { data, error } = await supabase
+        .from('budgets')
+        .select(`
+        *,
+        budget_categories (
+            category_id,
+            categories (*)
+        )
+`)
+        .eq('user_id', user_id)
+        .eq('is_active', true)
+        .order('created_at', { ascending: false });
+    if (error) throw error;
+    return data;
+}
+export async function updateBudget(budgetId: string, payload: IUpdateBudget, user_id: string) {
+    const { categories_ids, ...budgetData } = payload;
+    const { error } = await supabase
+        .from('budgets')
+        .update({ ...budgetData, updated_at: new Date().toISOString() })
+        .eq('id', budgetId)
+        .eq('is_active', true)
+        .eq('user_id', user_id)
+    if (error) throw error;
+
+    const { error: deleteRelationError } = await supabase
+        .from('budget_categories')
+        .delete()
+        .eq('budget_id', budgetId);
+    if (deleteRelationError) throw deleteRelationError;
+
+    if (categories_ids && categories_ids.length > 0) {
+        const relations = categories_ids.map((category_id) => (
+            {
+                budget_id: budgetId,
+                category_id
+            }
+        ))
+        const { error: insertRelationError } = await supabase
+            .from('budget_categories')
+            .insert(relations);
+
+        if (insertRelationError) throw insertRelationError;
+
+    }
+}
+
+export async function deleteBudget(budgetId: string, user_id: string) {
+    const { error } = await supabase
+        .from('budgets')
+        .update({ is_active: false })
+        .eq('id', budgetId)
+        .eq('user_id', user_id)
+    if (error) throw error;
+}
---
+++ src/types/IBudget.ts
@@ -0,0 +1,30 @@
+export interface IBudget {
+    user_id: string;
+    limit_amount: number;
+    name: string;
+    description?: string;
+    start_date: string;
+    end_date: string;
+    period: TPeriod;
+    created_at: string;
+    updated_at: string;
+    is_active: boolean;
+}
+export interface ICreateBudget {
+    limit_amount: number;
+    name: string;
+    description?: string;
+    period: TPeriod;
+    start_date: string;
+    end_date: string;
+    categories_ids: string[]
+}
+export interface IUpdateBudget{
+    name?: string;
+    description?: string;
+    period?: TPeriod;
+    start_date?: string;
+    end_date?: string;
+    categories_ids?: string[]
+}
+export type TPeriod = 'weekly' | 'monthly' | 'yearly' | 'daily';
