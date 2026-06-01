fc98739b6993360dac6ce3de502706dda963e736
Davidbeckham2005 <dinhhoangkhamtb04@gmail.com>
2026-06-01 11:21:28 +0700

    feat: more icons

--- src/Features/Dashbroad.tsx
+++ src/Features/Dashbroad.tsx
@@ -12,8 +12,6 @@ import ListTransaction from '../component/Transactions/ListTransaction';
 import ReportTransaction from '../component/Report/Report_form';
 import BudgetPage from '../component/Budget/Budget_list';
 import { useBudgetStore } from '../store/useBudgetStore';
-
-
 export default function DashBroad() {
     const { fetchCategories, isLoading } = useCategoryStore();
     const { fetchTransactions, isLoading: isTransactionsLoading } = useTransactionStore();
@@ -29,16 +27,12 @@ export default function DashBroad() {
     if (isLoading || isTransactionsLoading || isBudgetsLoading || isDeactivateLoading) {
         return <Loading></Loading>;
     }
-
-
     return (
         <div className="min-h-screen bg-white max-w-6xl w-full mx-auto *:bg-slate-50 rounded-3xl overflow-hidden shadow-xl border border-gray-100 ">
             <div className="grid grid-cols-1 lg:grid-cols-12">
                 <div className="lg:col-span-12 text-white">
                     <NavBar tab={tab} setTab={setTab}></NavBar>
-
                 </div>
-
                 <main className="lg:col-span-12 p-4 md:p-8 max-w-6xl w-full mx-auto">
                     {tab === 'category' && <ListCategory></ListCategory>}
                     {tab === 'lich' && <ListTransaction></ListTransaction>}
